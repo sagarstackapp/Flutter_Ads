87f031f721e39c2626b7c8a66ec5a009dbb8f4e5
Sagar Stackapp <sagaranghan.stackapp@gmail.com>
2022-01-22 15:41:09 +0530

    Add OneSignal Notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
+import 'dart:developer';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ads/common/constant/color_res.dart';
 import 'package:flutter_ads/screen/splash_screen/splash_screen.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+  OneSignal.shared.setAppId('65db3d90-202c-4b6e-99ac-08af3059ffba');
+  OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+    log('Accepted permission --> $accepted');
+  });
   MobileAds.instance.initialize();
   RequestConfiguration configuration = RequestConfiguration(testDeviceIds: [
     'BB87F960EF0F1760DCD2535EA14A3529',
